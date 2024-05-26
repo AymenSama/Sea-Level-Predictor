93d47b50230347006385398848ec22fd4dd51335
Aymen Sellami <Aymen.Sellami@hhu.de>
2024-05-26 19:49:35 +0200

    Plot best fit lines

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -7,14 +7,18 @@ def draw_plot():
     # Read data from file
     df = pd.read_csv("epa-sea-level.csv")
     # Create scatter plot
-    x_axis, y_axis = "Year", "CSIRO Adjusted Sea Level"
-    plt.scatter(x=df[x_axis], y=df[y_axis])
-    plt.xlabel(x_axis)
-    plt.ylabel(y_axis)
+    x, y = df["Year"], df["CSIRO Adjusted Sea Level"]
+    plt.scatter(x=x, y=y)
     # Create first line of best fit
-
+    res = linregress(x, y)
+    x = pd.Series(range(x[0], 2051))
+    plt.plot(x, res.intercept + res.slope * x)
     # Create second line of best fit
-
+    start_year = 2000
+    x, y = df[df["Year"] >= start_year]["Year"], df[df["Year"] >= start_year]["CSIRO Adjusted Sea Level"]
+    res = linregress(x, y)
+    x = pd.Series(range(start_year, 2051))
+    plt.plot(x, res.intercept + res.slope * x)
     # Add labels and title
 
     # Save plot and return data for testing (DO NOT MODIFY)
